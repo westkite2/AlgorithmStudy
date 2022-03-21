c30748f9e412366133cf83093b1d242226621fe1
westkite2 <westkite2@gmail.com>
2022-03-21 22:07:49 +0900

    second commit

--- Brute force/백준6064_카잉달력.py
+++ Brute force/백준6064_카잉달력.py
@@ -1,19 +1,16 @@
-#시간초과
+# x + Mm = y + Nn이다. (m, n은 각각 M, N을 더한 횟수)
+# 이때 (y + Nn)%N = y 인데, 위의 식에 따라 대입하면 (x + Mm)%N = y도 성립한다.
+# 이는 곧 ((x+Mm)-y)%N = 0을 의미한다.
+
+def year(M, N, x, y):
+    while x <= M * N:
+        if (x-y)%N == 0:
+            return x
+        x += M
+    return -1
+    
 T = int(input())
 for _ in range(T):
     M, N, x, y = map(int, input().split())
-    cx, cy, result = 1, 1, 1
-    while True:
-        if cx == x and cy == y:            
-            break
-        if cx == M and cy == N:
-            result = -1
-            break
-        cx += 1
-        cy += 1
-        result += 1
-        if cx > M:
-            cx = 1
-        if cy > N:
-            cy = 1
-    print(result)
+    print(year(M, N, x, y))
+    
