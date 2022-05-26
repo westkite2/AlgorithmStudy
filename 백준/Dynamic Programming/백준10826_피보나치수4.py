d9e836cf2ecac3d5cf5511bd0519ba6daae07681
westkite2 <westkite2@gmail.com>
2022-05-26 12:37:38 +0900

    second solution

--- 백준/Dynamic Programming/백준10826_피보나치수4.py
+++ 백준/Dynamic Programming/백준10826_피보나치수4.py
@@ -1,7 +1,5 @@
 n = int(input())
-def solution(n):
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a+b
-    return a
-print(solution(n))
+x, y = 0, 1
+for i in range(n):
+    x, y = y, x+y
+print(x)
