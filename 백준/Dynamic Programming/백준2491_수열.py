76405a6e15f4330a6030c1b0810972f42425ea62
westkite2 <westkite2@gmail.com>
2022-05-27 09:38:07 +0900

    second solution

--- 백준/Dynamic Programming/백준2491_수열.py
+++ 백준/Dynamic Programming/백준2491_수열.py
@@ -1,14 +1,10 @@
 n = int(input())
-nums = list(map(int, input().split()))
-result = 1
-
-rise = [1] * n
-fall = [1] * n
-
+num = list(map(int, input().split()))
+rise = [1]*n
+fall = [1]*n
 for i in range(1, n):
-    if nums[i-1] <= nums[i]:
+    if num[i-1] <= num[i]:
         rise[i] = rise[i-1] + 1
-    if nums[i-1] >= nums[i]:
+    if num[i-1] >= num[i]:
         fall[i] = fall[i-1] + 1
-
 print(max(max(rise), max(fall)))
