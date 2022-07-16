0905e6dcd52a90636d12d1c6b593b5fa2d77adf2
westkite2 <westkite2@gmail.com>
2022-07-16 09:51:34 +0900

    second solution

--- 프로그래머스/2018KakaoBlindRecruitment/[1차]비밀지도.py
+++ 프로그래머스/2018KakaoBlindRecruitment/[1차]비밀지도.py
@@ -1,25 +1,8 @@
-#첫 번째 풀이
-def solution(n, arr1, arr2):
-    answer = [""]*n
-    for i in range(n):
-        for j in range(n):
-            if arr1[i]%2 == 0 and arr2[i]%2 == 0:
-                answer[i] = " " + str(answer[i])
-            else:
-                answer[i] = "#" + str(answer[i])
-            arr1[i] = arr1[i]//2
-            arr2[i] = arr2[i]//2
-    return answer
-
-
-#두 번째 풀이
-
 def solution(n, arr1, arr2):
     answer = []
     for i, j in zip(arr1, arr2):
-        num = str(bin(i|j))[2:]
-        num = num.rjust(n,"0")
-        num = num.replace("0"," ")
-        num = num.replace("1","#")
-        answer.append(num)
+        tmp = bin(i|j)[2:].rjust(n, "0")
+        tmp = tmp.replace("1", "#")
+        tmp = tmp.replace("0", " ")
+        answer.append(tmp)
     return answer
