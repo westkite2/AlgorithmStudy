36262f2fe174dfe2baa650b2b996bfe1d2e5c6e0
westkite2 <westkite2@gmail.com>
2022-07-17 16:51:31 +0900

    second solution

--- 프로그래머스/2018KakaoBlindRecruitment/[1차]다트게임.py
+++ 프로그래머스/2018KakaoBlindRecruitment/[1차]다트게임.py
@@ -1,26 +1,23 @@
-import math
 def solution(dartResult):
     answer = 0
-    nums = []*3
+    nums = []
     j = -1
     dartResult = dartResult.replace('10', 'x')
     for i in dartResult:
-        if i.isnumeric():
-            j += 1
+        if i.isnumeric() == True:
             nums.append(int(i))
-        elif i == 'x':
             j += 1
+        elif i == 'x':
             nums.append(10)
-        elif i == 'S':
-            nums[j] = pow(nums[j], 1)
+            j += 1
         elif i == 'D':
             nums[j] = pow(nums[j], 2)
-        elif i == "T":
+        elif i == 'T':
             nums[j] = pow(nums[j], 3)
         elif i == "*":
-            nums[j] *= 2
-            if j!=0:
-                nums[j-1] *= 2
+            nums[j] = nums[j] * 2
+            if j > 0:
+                nums[j-1] = nums[j-1] * 2
         elif i == "#":
             nums[j] *= -1
     answer = sum(nums)
