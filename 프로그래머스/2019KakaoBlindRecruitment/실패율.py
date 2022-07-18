70d1ccdc7681d53dcc13c66fbe758a527a60e0f5
westkite2 <westkite2@gmail.com>
2022-07-18 13:55:36 +0900

    second solution

--- 프로그래머스/2019KakaoBlindRecruitment/실패율.py
+++ 프로그래머스/2019KakaoBlindRecruitment/실패율.py
@@ -1,13 +1,12 @@
 def solution(N, stages):
     answer = {}
-    users = len(stages)
-
-    for i in range(1, N+1):
-        stay = stages.count(i)
-        if stay != 0:
-            answer[i] = stay/users 
-            users -= stay
+    lens = len(stages)
+    for stage in range(1,N+1):
+        tmp = stages.count(stage)
+        if tmp != 0:
+            answer[stage] = stages.count(stage) / lens
+            lens -= tmp
         else:
-            answer[i] = 0
-    answer = sorted(answer, key = lambda i : answer[i], reverse = True)
+            answer[stage] = 0
+    answer = sorted(answer, key = lambda x: answer[x], reverse = True)
     return answer
