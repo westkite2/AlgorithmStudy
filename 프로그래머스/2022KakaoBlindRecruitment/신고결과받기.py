2db436def571f51c02c7dd373b26db8006af5270
westkite2 <westkite2@gmail.com>
2022-07-20 14:22:05 +0900

    second solution

--- 프로그래머스/2022KakaoBlindRecruitment/신고결과받기.py
+++ 프로그래머스/2022KakaoBlindRecruitment/신고결과받기.py
@@ -1,33 +1,10 @@
-from collections import defaultdict
-
 def solution(id_list, report, k):
-    answer = []
-    result = defaultdict(int)
-    accused = defaultdict(list)
-    count = defaultdict(int)
-    for case in set(report):
-        a, b = case.split()
-        accused[a] += [b]
-        count[b] += 1
-    for user in id_list:
-        if count[user] >= k:
-            for users in id_list:
-                if user in accused[users]:
-                    result[users] += 1
-    for user in id_list:
-        answer.append(result[user])
-    return answer
-
-#other solution
-'''
-def solution(id_list, report, k):
-    answer = [0] * len(id_list)
-    accused = {x:0 for x in id_list}
-    
-    for case in set(report):
-        accused[case.split()[1]] += 1
-    for case in set(report):
-        if accused[case.split()[1]] >= k:
+    answer = [0]*len(id_list)
+    accused = [0]*len(id_list)
+    report = set(report)
+    for case in report:
+        accused[id_list.index(case.split()[1])] += 1
+    for case in report:
+        if accused[id_list.index(case.split()[1])] >= k:
             answer[id_list.index(case.split()[0])] += 1
-    return answer
-'''
+    return answers
